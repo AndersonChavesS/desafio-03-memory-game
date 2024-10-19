42492604f018e07377a25865123d79fb08727ec3
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-19 16:14:40 -0300

    feat: criado os cards dinamicamente

---
+++ src/JS/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+  ' 🎠',
+  ' 🎠',
+  ' 🎃',
+  ' 🎃',
+  ' ⚽',
+  ' ⚽',
+  ' 💸',
+  ' 💸',
+  ' 😋',
+  ' 😋',
+  ' 🐱‍🏍',
+  ' 🐱‍🏍',
+  ' 🐲',
+  ' 🐲',
+  ' 🐎',
+  ' 🐎',
+];
+let openCards = [];
+
+let shuffledEmojis = emojis.sort(() => Math.random() > 0.5 ? 2 : -1);
+
+for(let i = 0; i < emojis.length;i++) {
+  let box = document.createElement('div');
+  box.className = 'item';
+  box.innerHTML = shuffledEmojis[i];
+  document.querySelector('.game').appendChild(box);
+}
