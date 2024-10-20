277ebeda336c241e13eb9f0777071ffb2ac0dbeb
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-10-19 22:03:08 -0300

    feat: adicionando efeito de rotate nos cards; import de favicon e adicionado favicon

--- src/JS/engine.js
+++ src/JS/engine.js
@@ -18,11 +18,24 @@ const emojis = [
 ];
 let openCards = [];
 
-let shuffledEmojis = emojis.sort(() => Math.random() > 0.5 ? 2 : -1);
+let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i = 0; i < emojis.length;i++) {
+for (let i = 0; i < emojis.length; i++) {
   let box = document.createElement('div');
   box.className = 'item';
   box.innerHTML = shuffledEmojis[i];
+  box.onclick = handleClick;
   document.querySelector('.game').appendChild(box);
 }
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add('boxOpen');
+    openCards.push(this);
+  }
+  if (openCards.lenght === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+function checkMatch() {}
